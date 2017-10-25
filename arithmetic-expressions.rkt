2c7efebfb9cf90fa1eaaf5fe4af52a527d6430af
Seab <honeyboyseab@gmail.com>
2017-10-25 18:52:05 -0400

    add gitignore to not track executable files

---
+++ arithmetic-expressions.rkt
@@ -0,0 +1,12 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname arithmetic-expressions) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+
+;PROBLEM:
+
+;Write two expressions that multiply the numbers 3, 5 and 7. 
+;The first should take advantage of the fact that * can accept more than 2 arguments. 
+;The second should build up the result by first multiplying 3 times 5 and then multiply the result of that by 7. 
+
+(* 3 5 7)
+(* (* 3 5) 7)
