eda9e6b39bbd9c77926210aae96fcadec8d290da
Seab <honeyboyseab@gmail.com>
2017-10-25 20:07:30 -0400

    compare image width and height using primitives

---
+++ compare-images.rkt
@@ -0,0 +1,24 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname compare-images) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+(require 2htdp/image)
+
+;; compare-images
+;
+;Based on the two constants provided, write three expressions to determine whether: 
+
+(define IMAGE1 (rectangle 10 15 "solid" "red"))
+(define IMAGE2 (rectangle 15 10 "solid" "red"))
+
+
+;1) IMAGE1 is taller than IMAGE2
+
+(> (image-height IMAGE1) (image-height IMAGE2))
+
+;2) IMAGE1 is narrower than IMAGE2
+
+(< (image-width IMAGE1) (image-width IMAGE2))
+
+;3) IMAGE1 has both the same width AND height as IMAGE2
+
+(and (= (image-width IMAGE1) (image-width IMAGE2) (image-height IMAGE1) (image-height IMAGE2))
