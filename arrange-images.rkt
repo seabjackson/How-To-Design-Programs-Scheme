95fa0a02c3dcca4d314fad8892598b699f095ba6
Seab <honeyboyseab@gmail.com>
2017-11-15 21:20:24 -0500

    made constants for easier testing

--- arrange-images.rkt
+++ arrange-images.rkt
@@ -19,20 +19,24 @@
 
 (define BLANK (square 0 "solid" "white"))
 
+;; for testing purposes
+(define REC1 (rectangle 10 15 "solid" "red"))
+(define REC2 (rectangle 25 15 "solid" "green"))
+
 ;; ListOfImages is one of: 2 cases
 ;; - empty
 ;; - compound: (cons Image ListOfImage)
 ;; interp. An arbitrary number of images
 
 (define LO1 empty)
-(define L02 (cons (rectangle 10 15 "solid" "red")
-                  (cons (rectangle 25 15 "solid" "green") empty)))
+(define L02 (cons REC1
+                  (cons REC2 empty)))
 #;
- (define (fn-for-loi loi)
-     (cond [(empty? loi) (...)]
-           [else
-            (... (first loi)
-                 (fn-for-loi (rest loi)))]))
+(define (fn-for-loi loi)
+  (cond [(empty? loi) (...)]
+        [else
+         (... (first loi)
+              (fn-for-loi (rest loi)))]))
 
 ;; Functions:
 
@@ -40,16 +44,16 @@
 ;; sort images in increasing order of size and display them from left to right
 ;; sort images in increasing order of size then display them
 
-(check-expect (arrange-images (cons (rectangle 10 15 "solid" "red")
-                                    (cons (rectangle 25 15 "solid" "green") empty)))
-              (beside (rectangle 10 15 "solid" "red")
-                      (rectangle 25 15 "solid" "green")
+(check-expect (arrange-images (cons REC1
+                                    (cons REC2 empty)))
+              (beside REC1
+                      REC2
                       BLANK))
 
-(check-expect (arrange-images (cons (rectangle 25 15 "solid" "green")
-                                    (cons (rectangle 10 15 "solid" "red") empty)))
-              (beside (rectangle 10 15 "solid" "red")
-                      (rectangle 25 15 "solid" "green")
+(check-expect (arrange-images (cons REC2
+                                    (cons REC1 empty)))
+              (beside REC1
+                      REC2
                       BLANK))
 
 ;(define (arrange-images loi) BLANK); stub
@@ -61,21 +65,46 @@
 ;; ListOfImage -> Image
 ;; place images next to each other in order of list
 (check-expect (display-images empty) BLANK)
-(check-expect (display-images  (cons (rectangle 10 15 "solid" "red")
-                                     (cons (rectangle 25 15 "solid" "green") empty)))
-              (beside (rectangle 10 15 "solid" "red")
-                      (rectangle 25 15 "solid" "green")
+(check-expect (display-images  (cons REC1
+                                     (cons REC2 empty)))
+              (beside REC1
+                      REC2
                       BLANK))
 
 ;(define (display-images loi) BLANK); stub
 
 (define (display-images loi)
-     (cond [(empty? loi) BLANK]
-           [else
-            (beside (first loi)
+  (cond [(empty? loi) BLANK]
+        [else
+         (beside (first loi)
                  (display-images (rest loi)))]))
+
 
 ;; ListOfImage -> ListOfImage
 ;; prduce a sorted list of images in increasing order of size
-;; !!!
-(define (sort-images loi) loi); stub
+(check-expect (sort-images empty) empty)
+(check-expect (sort-images (cons REC1
+                                 (cons REC2 empty)))
+              (cons REC1
+                    (cons REC2 empty)))
+
+(check-expect (sort-images (cons REC2
+                                 (cons REC1 empty)))
+              (cons REC1
+                    (cons REC2 empty)))
+
+;(define (sort-images loi) loi); stub
+
+(define (sort-images loi)
+  (cond [(empty? loi) empty]
+        [else
+         (insert (first loi)
+              (sort-images (rest loi)))]))
+
+;; Image ListOfImage -> ListOfImage
+;; produce a new list of images with image in proper place
+;; Assume that list is already sorted
+
+
+(define (insert img list) list)
+
